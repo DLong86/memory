0e42248796c6b80a3d321a74cd1ec1d55b4d3eed
DLong86 <daveallenlong@gmail.com>
2023-06-01 14:41:42 +0900

    added new hook and questions

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,64 +1,10 @@
 import React, { useState, useEffect, useRef } from 'react';
 import useHoverLoop from '../hooks/useHoverLoop';
-import hobbies from '../db/hobbies';
-import sports from "../db/sports";
-import travel from "../db/travel"
+import useQuestionText from '../hooks/useQuestionText';
 
 const Cards = () => {
   const {hoverIndex, startHoverLoop} = useHoverLoop()
-
-  const [text, setText] = useState({
-    hobbies: "hobbies",
-    hobbies2: "hobbies",
-    sports: "sports",
-    sports2: "sports",
-    travel: "travel",
-    travel2: "travel",
-    health: "health",
-    health: "health",
-    life: "life",
-    life2: "life",
-    general: "general",
-    general2: "general",
-  })
-
-  const handleHobbies = (e) => {
-    const item = hobbies[Math.floor(Math.random() * hobbies.length)].question;
-    const divId = e.target.id 
-
-    if (divId === 'hobbies' || divId === 'hobbies2') {
-      setText((prevTexts) => ({
-        ...prevTexts,
-        [divId]: prevTexts[divId] === 'hobbies' ? item : 'hobbies',
-      }));
-    }
-}
-
-const handleSports = (e) => {
-  const item = sports[Math.floor(Math.random() * sports.length)].question;
-  console.log(item);
-  const divId = e.target.id 
-
-  if (divId === 'sports' || divId === 'sports2') {
-    setText((prevTexts) => ({
-      ...prevTexts,
-      [divId]: prevTexts[divId] === 'sports' ? item : 'sports',
-    }));
-  }
-}
-
-const handleTravel = (e) => {
-  const item = travel[Math.floor(Math.random() * travel.length)].question;
-  console.log(item);
-  const divId = e.target.id 
-
-  if (divId === 'travel' || divId === 'travel2') {
-    setText((prevTexts) => ({
-      ...prevTexts,
-      [divId]: prevTexts[divId] === 'travel' ? item : 'travel',
-    }));
-  }
-}
+  const { text, handleHobbies, handleSports, handleTravel, handleGeneral, handleLife, handleHealth } = useQuestionText();
 
   return (
     <div className="">
@@ -67,15 +13,15 @@ const handleTravel = (e) => {
           <div id='hobbies' className={`bg-[#99c5df] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 0 ? ' bg-black' : ''}`} onClick={handleHobbies}>{text["hobbies"]}</div>
           <div id='sports' className={`bg-[#f6b4bf] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 1 ? 'bg-black' : ''}`} onClick={handleSports}>{text["sports"]}</div>
           <div id='travel' className={`bg-[#f8df81] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 2 ? 'bg-black' : ''}`} onClick={handleTravel}>{text["travel"]}</div>
-          <div className={`bg-[#9bd0b7] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 3 ? 'bg-black' : ''}`}>Health</div>
+          <div id='health' className={`bg-[#9bd0b7] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 3 ? 'bg-black' : ''}`} onClick={handleHealth}>{text["health"]}</div>
           <div id='hobbies2' className={`bg-[#99c5df] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 4 ? 'bg-black' : ''}`} onClick={handleHobbies}>{text["hobbies2"]}</div>
-          <div className={`bg-[#cc89d6] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 5 ? 'bg-black' : ''}`}>Life</div>
-          <div className={`bg-[#f2687b] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 6 ? 'bg-black' : ''}`}>General</div>
+          <div id='life' className={`bg-[#cc89d6] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 5 ? 'bg-black' : ''}`} onClick={handleLife}>{text["life"]}</div>
+          <div id='general' className={`bg-[#f2687b] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 6 ? 'bg-black' : ''}`} onClick={handleGeneral}>{text["general"]}</div>
           <div id='travel2' className={`bg-[#f8df81] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 7 ? 'bg-black' : ''}`} onClick={handleTravel}>{text["travel2"]}</div>
           <div id='sports2' className={`bg-[#f6b4bf] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 8 ? 'bg-black' : ''}`} onClick={handleSports}>{text["sports2"]}</div>
-          <div className={`bg-[#cc89d6] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 9 ? 'bg-black' : ''}`}>Life</div>
-          <div className={`bg-[#9bd0b7] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 10 ? 'bg-black' : ''}`}>Health</div>
-          <div className={`bg-[#f2687b] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 11 ? 'bg-black' : ''}`}>General</div>
+          <div id='life2' className={`bg-[#cc89d6] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 9 ? 'bg-black' : ''}`} onClick={handleLife}>{text["life2"]}</div>
+          <div id='health2' className={`bg-[#9bd0b7] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 10 ? 'bg-black' : ''}`} onClick={handleHealth}>{text["health2"]}</div>
+          <div id='general2' className={`bg-[#f2687b] rounded-md flex items-center justify-center cursor-pointer duration-100 px-2 ${hoverIndex === 11 ? 'bg-black' : ''}`} onClick={handleGeneral}>{text["general2"]}</div>
       </div>
     </div>
   )
--- src/db/sports.jsx
+++ src/db/sports.jsx
@@ -2,7 +2,7 @@ const sports = [
     { question: "What is your favourite sport to watch on TV?" },
     { question: "What was your favourite sport five years ago?" },
     { question: "Ask the person next to you what their favourite sports are." },
-    {question: "What do sports do you like to play?"},
+    {question: "What sports do you like to play?"},
     {question: "Which sports person do you admire and why?"},
     {question: "What sport would you most like to try?"},
     {question: "What is your best sporting memory?"},
